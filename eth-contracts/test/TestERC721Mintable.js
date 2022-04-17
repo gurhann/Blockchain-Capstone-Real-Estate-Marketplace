489779886e51e7693cb6634c8e2b1bd9d6788fcb
Gürhan Küçük <gurhan.kucuk@yandex.com>
2022-04-17 09:35:22 +0300

    fix

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -6,76 +6,64 @@ var ERC721Mintable = artifacts.require('ERC721Mintable');
 
 contract('TestERC721Mintable', accounts => {
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
-    const baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
-    const zeroAddress = '0x0000000000000000000000000000000000000000';
+    const first_account = accounts[0];
+    const second_account = accounts[1];
+    const tokenAddress = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+    const emptyAddress = '0x0000000000000000000000000000000000000000';
 
     describe('match erc721 spec', function () {
-        const tokensIds = [11, 22, 33, 44, 55, 66, 77, 88, 99, 101];
+        const tokens = [11, 22, 33, 44, 55, 66, 77, 88, 99, 101];
         beforeEach(async function () { 
-            this.contract = await ERC721Mintable.new("GK_ERC721Mintable", "GK_721M", {from: account_one});
+            this.contract = await ERC721Mintable.new("GK_ERC721Mintable", "GK_721M", {from: first_account});
            
             // TODO: mint multiple tokens
-            for (let i = 0; i < tokensIds.length - 1; i++) {
-                await this.contract.mint(accounts[i + 1], tokensIds[i], {from: account_one});
+            for (let i = 0; i < tokens.length - 1; i++) {
+                await this.contract.mint(accounts[i + 1], tokens[i], {from: first_account});
             }
-            await this.contract.mint(accounts[tokensIds.length -1], tokensIds[tokensIds.length-1], {from: account_one});
+            await this.contract.mint(accounts[tokens.length -1], tokens[tokens.length-1], {from: first_account});
         })
 
-        it('should return total supply', async function () { 
+        it('get totken balance should be correct', async function () { 
+            const accountBalance = await this.contract.balanceOf(accounts[3]);
+            expect(Number(accountBalance)).to.equal(1);
+        })
+
+        it('tokenAddress should be correct', async function () { 
+            const expectedTokenAddress = await this.contract.tokenURI(tokens[3]); 
+            expect(expectedTokenAddress).to.deep.equal(`${tokenAddress}${tokens[3]}`);
+
+        })
+
+        it('total suppy should be correct', async function () { 
             const totalSupply = await this.contract.totalSupply.call({from: accounts[9]});
-            expect(Number(totalSupply)).to.equal(tokensIds.length);
+            expect(Number(totalSupply)).to.equal(tokens.length);
         })
 
-        it('should get token balance', async function () { 
-            const acc3Balance = await this.contract.balanceOf(accounts[3]);
-            expect(Number(acc3Balance)).to.equal(1);
-
-            const acc9Balance = await this.contract.balanceOf(accounts[9]);     
-            expect(Number(acc9Balance)).to.equal(2);
-        })
-
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            const token3Uri = await this.contract.tokenURI(tokensIds[3]); 
-            expect(token3Uri).to.deep.equal(`${baseTokenURI}${tokensIds[3]}`);
- 
-            const token6Uri = await this.contract.tokenURI(tokensIds[6]); 
-            expect(token6Uri).to.deep.equal(`${baseTokenURI}${tokensIds[6]}`);
- 
-            const token9Uri = await this.contract.tokenURI(tokensIds[9]); 
-            expect(token9Uri).to.deep.equal(`${baseTokenURI}${tokensIds[9]}`);
-        })
-
-        it('should transfer token from one owner to another', async function () { 
-            await this.contract.approve(accounts[9], tokensIds[7], {from: accounts[8]});
-            let tx = await this.contract.transferFrom(accounts[8], accounts[9], tokensIds[7], {from: accounts[9]});
-            truffleAssert.eventEmitted(tx, 'Transfer', (ev) => {
-                return expect(ev.from).to.deep.equal(accounts[8]) 
-                       && expect(ev.to).to.equal(accounts[9])
-                       && expect(Number(ev.tokenId)).to.equal(tokensIds[7]);
+        it('trasfer can be done', async function () { 
+            await this.contract.approve(accounts[6], tokens[3], {from: accounts[4]});
+            let transaction = await this.contract.transferFrom(accounts[4], accounts[6], tokens[3], {from: accounts[6]});
+            truffleAssert.eventEmitted(transaction, 'Transfer', (ev) => {
+                return expect(ev.from).to.deep.equal(accounts[4]) 
+                       && expect(ev.to).to.equal(accounts[6])
+                       && expect(Number(ev.tokenId)).to.equal(tokens[3]);
             });
 
-            expect(await this.contract.ownerOf(tokensIds[7])).to.equal(accounts[9]);
-            expect(Number(await this.contract.balanceOf(accounts[9]))).to.equal(3);
-            expect(Number(await this.contract.balanceOf(accounts[8]))).to.equal(0);
-            expect(await this.contract.getApproved(tokensIds[7])).to.equal(zeroAddress);
+            expect(await this.contract.ownerOf(tokens[3])).to.equal(accounts[6]);
         })
     });
 
     describe('have ownership properties', function () {
         beforeEach(async function () { 
-            this.contract = await ERC721Mintable.new("GK_ERC721Mintable", "GK_721M", {from: account_one});
-            currentOwner = account_one;
+            this.contract = await ERC721Mintable.new("GK_ERC721Mintable", "GK_721M", {from: first_account});
+            currentOwner = first_account;
         })
 
-        it('should fail when minting when address is not contract owner', async function () { 
-            await expectToRevert(this.contract.mint(account_two, 12, {from: account_two}), 'Caller is not the contract owner');
+        it('if address is not contract owner then should be fail', async function () { 
+            await expectToRevert(this.contract.mint(second_account, 12, {from: second_account}), 'Caller is not the contract owner');
         })
 
         it('should return contract owner', async function () { 
-            expect(await this.contract.owner({from: account_one})).to.equal(currentOwner); ;
+            expect(await this.contract.owner({from: first_account})).to.equal(currentOwner); ;
         })
 
     });
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -3,27 +3,26 @@
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
 const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 const Verifier = artifacts.require('Verifier');
-const truffleAssert = require("truffle-assertions");
 var proofJson = require('./proof.json');
 
 contract('TestSolnSquareVerifier', accounts => {
-  describe('can verify soultion and mint token', () => {
-    const account_one = accounts[0];
-    const account_two = accounts[1];
+  describe('verify soultion and mint token', () => {
+    const first_account = accounts[0];
+    const second_account = accounts[1];
     let verifier;
     let solnSquareVerifier;
 
     beforeEach(async () => {
-      verifier = await Verifier.new({ from: account_one });
-      solnSquareVerifier = await SolnSquareVerifier.new(verifier.address,"GK_ERC721Mintable", "GK_721M", {from: account_one});
+      verifier = await Verifier.new({ from: first_account });
+      solnSquareVerifier = await SolnSquareVerifier.new(verifier.address,"GK_ERC721Mintable", "GK_721M", {from: first_account});
     });
     it('can add a new solution', async () => {
       const { proof: { a, b, c }, inputs: input } = proofJson;
       let key = await solnSquareVerifier.generateKey.call(a, b, c, input);
       console.log("key", key);
-      let result = await solnSquareVerifier.addSolution(1, account_two, key);
+      let result = await solnSquareVerifier.addSolution(1, second_account, key);
 
-      assert.equal(result.logs[0].event, 'SolutionAdded', "Error: no event emitted.");
+      assert.equal(result.logs[0].event, 'SolutionAdded', "no event emitted");
     });
     it("can mint an ERC721 token", async() => {
 
@@ -31,12 +30,12 @@ contract('TestSolnSquareVerifier', accounts => {
 
       let supplyBefore = (await solnSquareVerifier.totalSupply.call()).toNumber();
 
-      await solnSquareVerifier.mintToken(account_two, 2, a, b, c, input, {from: account_one});
+      await solnSquareVerifier.mintToken(second_account, 2, a, b, c, input, {from: first_account});
 
       let supplyAfter = (await solnSquareVerifier.totalSupply.call()).toNumber();
       let difference = supplyAfter - supplyBefore;
 
-      assert.equal(difference, 1, "Error: an ERC721 token cannot be minted for contract - SolnSquareVerifier");
+      assert.equal(difference, 1, "token cannot be minted for contract");
     });
   });
 });
